508feb7d5cc33d7c4ff86ea9398b362423c1c226
Ruslan Dautkhanov <dautkhanov@gmail.com>
2015-09-13 23:33:09 -0600

    test

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,8 +12,9 @@
 """
     
 import sys
-from time import time
 sys.path.append("../tools/")
+
+from time import time
 from email_preprocess import preprocess
 
 
@@ -22,13 +23,27 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+print "preprocessing train and test data sets.."
+t0 = time()
 features_train, features_test, labels_train, labels_test = preprocess()
-
+print "pre-processing time: ", round(time()-t0, 3), "s"
 
 
 
 #########################################################
 ### your code goes here ###
+
+from sklearn.naive_bayes import GaussianNB as GNB
+clf = GNB()
+
+t0 = time()
+print "training..."
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
+
+t0 = time()
+print "test score (accuracy): ", clf.score(features_test, labels_test)
+print "scoring time: ", round(time()-t0, 6), "s"
 
 
 #########################################################
